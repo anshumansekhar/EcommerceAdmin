d03ea37db327348b406d52de21b998394101cd8f
anshumansekhar <anshumansekhar@hotmail.com>
2021-04-19 12:18:57 +0530

    Updates

---
+++ precache-manifest.55d0c16ddbcc97106d8442188663aad5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "694c4d77267155d5c78c202662f012bb",
+    "url": "/YouShopWeShipAdmin/index.html"
+  },
+  {
+    "revision": "6e1c8a0fce379b8f4cdb",
+    "url": "/YouShopWeShipAdmin/static/css/2.6b8d264a.chunk.css"
+  },
+  {
+    "revision": "485f0173a1deb086fb9f",
+    "url": "/YouShopWeShipAdmin/static/css/main.2d3c9da3.chunk.css"
+  },
+  {
+    "revision": "6e1c8a0fce379b8f4cdb",
+    "url": "/YouShopWeShipAdmin/static/js/2.27ee501f.chunk.js"
+  },
+  {
+    "revision": "fcf8eb061440ed54defcf76ae7ffe82a",
+    "url": "/YouShopWeShipAdmin/static/js/2.27ee501f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "485f0173a1deb086fb9f",
+    "url": "/YouShopWeShipAdmin/static/js/main.b2753933.chunk.js"
+  },
+  {
+    "revision": "54ef0ed3eb236e22d0cb",
+    "url": "/YouShopWeShipAdmin/static/js/runtime-main.fd23ea16.js"
+  }
+]);
